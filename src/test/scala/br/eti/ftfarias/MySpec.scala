751df45a171d11c4e1c60dbfbba0118815535935
Felipe Farias <felipe.cofema@gmail.com>
2010-03-18 21:17:05 -0300

    test spec updated

--- src/test/scala/br/eti/ftfarias/MySpec.scala
+++ src/test/scala/br/eti/ftfarias/MySpec.scala
@@ -3,10 +3,11 @@ package br.eti.ftfarias
 import org.specs._
 import org.specs.runner.{ConsoleRunner, JUnit4}
 
-class MySpecTest extends JUnit4(MySpec)
-//class MySpecSuite extends ScalaTestSuite(MySpec)
-object MySpecRunner extends ConsoleRunner(MySpec)
 
+class MySpecTest extends JUnit4(helloWorld)
+//class MySpecSuite extends ScalaTestSuite(MySpec)
+//object MySpecRunner extends ConsoleRunner(helloWorld)
+/*
 object MySpec extends Specification {
   "This wonderful system" should {
     "save the world" in {
@@ -14,4 +15,16 @@ object MySpec extends Specification {
       list must beEmpty
     }
   }
+}
+*/
+
+object helloWorld extends Specification {
+	"This wonderful system" should {
+  "'hello world' has 11 characters" in {
+     "hello world".size must_== 11
+  }
+  "'hello world' matches 'h.* w.*'" in {
+     "hello world" must be matching("h.* w.*")
+  }
+	}
 }
